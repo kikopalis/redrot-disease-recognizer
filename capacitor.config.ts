2ca0ffa2c041f14563a31fa7e8e1e3c1aa2b64e4
Darwin Cabarrubias <darwin@thinkbitsolutions.com>
2024-04-27 14:34:54 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'redrot-disease-recognizer',
+  webDir: 'www'
+};
+
+export default config;
